b2a7475548ca78c34402b104bbccaac40d1de6df
Petrosz007 <andipeti2000@gmail.com>
2022-04-18 17:44:24 +0000

    Functionally complete S2

--- include/bimap2.hpp
+++ include/bimap2.hpp
@@ -55,25 +55,28 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
    public:
     using iterator_category = std::forward_iterator_tag;
     using difference_type = std::ptrdiff_t;
-    using value_type = std::pair<TId, TValue>;
+    using value_type = std::pair<const TId&, const TValue&>;
     using vector_pointer = typename std::vector<TValue>::const_iterator;
-    using pointer = std::pair<TId, TValue>*;    // or also value_type*
-    using reference = std::pair<TId, TValue>&;  // or also value_type&
+    using pointer = value_type*;    // or also value_type*
+    using reference = value_type&;  // or also value_type&
 
     const_iterator(vector_pointer ptr, vector_pointer begin)
-        : m_ptr(ptr), m_begin(begin) {}
-
-    std::pair<TId, TValue> operator*() const {
-      auto p = std::make_pair(std::distance(m_begin, m_ptr), std::move(*m_ptr));
-      return p;
+        : m_ptr(ptr), m_begin(begin) {
+      set_current();
     }
 
-    std::unique_ptr<std::pair<TId, TValue>> operator->() {
-      return std::make_unique<std::pair<TId, TValue>>(
-          std::distance(m_begin, m_ptr), std::move(*m_ptr));
+    const_iterator(const const_iterator& other)
+        : m_ptr(other.m_ptr), m_begin(other.m_begin) {
+      set_current();
     }
+
+    const value_type& operator*() const { return *m_current; }
+
+    const value_type* operator->() { return &*m_current; }
+
     const_iterator& operator++() {
       m_ptr++;
+      set_current();
       return *this;
     }
 
@@ -93,6 +96,13 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
    private:
     vector_pointer m_ptr;
     vector_pointer m_begin;
+    TId m_current_id;
+    std::optional<value_type> m_current;
+
+    void set_current() {
+      m_current_id = std::distance(m_begin, m_ptr);
+      m_current.emplace(m_current_id, *m_ptr);
+    }
   };
 
   // Iterators
@@ -117,14 +127,19 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
     return std::pair{it, true};
   }
 
-  std::pair<const_iterator, bool> emplace(TValue value) {
-    const auto& id = id_for_value(value);
-    if (id) {
-      auto id_it = const_iterator(id_map.begin() + *id, id_map.begin());
-      return std::pair{id_it, false};
-    }
+  template <typename... Args>
+  std::pair<const_iterator, bool> emplace(Args&&... args) {
+    // const auto& id = id_for_value(value);
+    // if (id) {
+    //   auto id_it = const_iterator(id_map.begin() + *id, id_map.begin());
+    //   return std::pair{id_it, false};
+    // }
 
-    id_map.emplace_back(std::move(value));
+    // if constexpr (std::is_copy_constructible<TValue>::value) {
+    id_map.emplace_back(std::forward<Args>(args)...);
+    // } else {
+    // id_map.emplace_back(args);
+    // }
     auto it = const_iterator(id_map.end() - 1, id_map.begin());
 
     return std::pair{it, true};
@@ -144,7 +159,7 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   }
 
   std::size_t erase(const TId id) {
-    if (id < id_map.size()) {
+    if (id >= id_map.size()) {
       return 0;
     }
 
@@ -156,8 +171,10 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   template <typename UnaryPredicate>
   std::size_t delete_all(UnaryPredicate predicate) {
     std::size_t deleted_count = 0;
-    for (TId i = 0; i < id_map.size(); ++i) {
-      if (predicate(id_map[i])) {
+    const auto size = id_map.size();
+
+    for (TId i = 0; i < size; ++i) {
+      if (predicate(id_map[i - deleted_count])) {
         delete_id_and_shift(i - deleted_count);
         ++deleted_count;
       }
@@ -180,7 +197,7 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
     return *id;
   }
 
-  const TValue& operator[](const TId id) const { return id_map.at(id); }
+  const TValue& operator[](const TId& id) const { return id_map.at(id); }
 
   const_iterator find(const TValue& value) const {
     const auto& id = id_for_value(value);
@@ -190,8 +207,10 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
     return const_iterator(id_map.begin() + *id, id_map.begin());
   }
 
-  const_iterator find(const TId id) const {
-    return const_iterator(id_map.begin() + id, id_map.begin());
+  template <typename... Args>
+  const_iterator find(Args... args) const {
+    const TValue value = TValue(std::forward<Args>(args)...);
+    return find(value);
   }
 
   template <typename UnaryPredicate>
--- test/section2_tests.hpp
+++ test/section2_tests.hpp
@@ -3,7 +3,7 @@
 #include <original_tests/f2.hpp>
 #include <sut.hpp>
 
-// TEST_CASE("[S2] Original Section 2 tests") { F2_advanced(); }
+TEST_CASE("[S2] Original Section 2 tests") { F2_advanced(); }
 
 TEST_CASE("[S2] All Tests") {
   SMFCounter::reset();
