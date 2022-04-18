02c89a918c12622d3f6d4144217e455e0b9f6d50
Petrosz007 <andipeti2000@gmail.com>
2022-04-18 14:57:50 +0000

    Not working implementation 🙃🔫🔫

--- include/bimap2.hpp
+++ include/bimap2.hpp
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <concepts>
+#include <cstddef>  // For std::ptrdiff_t
 #include <iterator>
+#include <iterator>  // For std::forward_iterator_tag
 #include <map>
 #include <memory>
 #include <ranges>
@@ -11,8 +13,7 @@
 template <typename TValue, std::integral TId = std::size_t>
 requires(!std::same_as<TValue, TId>) class id_bimap {
  private:
-  std::map<TId, TValue> id_map;
-  TId next_id;
+  std::vector<TValue> id_map;
 
   /**
    * @brief Deletes `id` and shifts every ID greather than this down by one. The
@@ -20,23 +21,12 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
    *
    * @param id Id to delete
    */
-  void delete_id_and_shift(const TId id) {
-    id_map.erase(id);
-
-    for (auto i = id + 1; i < next_id; ++i) {
-      auto node = id_map.extract(i);
-      --node.key();
-
-      id_map.insert(std::move(node));
-    }
-
-    --next_id;
-  }
+  void delete_id_and_shift(const TId id) { id_map.erase(id_map.begin() + id); }
 
   std::optional<TId> id_for_value(const TValue& value_to_search) const {
-    for (const auto& [key, value] : id_map) {
-      if (value == value_to_search) {
-        return key;
+    for (TId i = 0; i < id_map.size(); ++i) {
+      if (id_map[i] == value_to_search) {
+        return i;
       }
     }
 
@@ -44,7 +34,7 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   }
 
  public:
-  id_bimap() noexcept : id_map(), next_id() {}
+  id_bimap() noexcept : id_map() {}
 
   id_bimap(const id_bimap& other) noexcept = default;
 
@@ -60,23 +50,69 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
 
   using mapped_type = TValue;
   using key_type = TId;
-  using const_iterator = typename std::map<TId, TValue>::const_iterator;
+  // using const_iterator = typename std::vector<TValue>::const_iterator;
+  struct const_iterator {
+   public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = std::pair<TId, TValue>;
+    using vector_pointer = typename std::vector<TValue>::const_iterator;
+    using pointer = std::pair<TId, TValue>*;    // or also value_type*
+    using reference = std::pair<TId, TValue>&;  // or also value_type&
+
+    const_iterator(vector_pointer ptr, vector_pointer begin)
+        : m_ptr(ptr), m_begin(begin) {}
+
+    std::pair<TId, TValue> operator*() const {
+      auto p = std::make_pair(std::distance(m_begin, m_ptr), std::move(*m_ptr));
+      return p;
+    }
+
+    std::unique_ptr<std::pair<TId, TValue>> operator->() {
+      return std::make_unique<std::pair<TId, TValue>>(
+          std::distance(m_begin, m_ptr), std::move(*m_ptr));
+    }
+    const_iterator& operator++() {
+      m_ptr++;
+      return *this;
+    }
+
+    const_iterator operator++(int) {
+      const_iterator tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    friend bool operator==(const const_iterator& a, const const_iterator& b) {
+      return a.m_ptr == b.m_ptr;
+    };
+    friend bool operator!=(const const_iterator& a, const const_iterator& b) {
+      return a.m_ptr != b.m_ptr;
+    };
+
+   private:
+    vector_pointer m_ptr;
+    vector_pointer m_begin;
+  };
 
   // Iterators
-  const_iterator begin() const noexcept { return id_map.begin(); }
-  const_iterator end() const noexcept { return id_map.end(); }
+  const_iterator begin() const noexcept {
+    return const_iterator(id_map.begin(), id_map.begin());
+  }
+  const_iterator end() const noexcept {
+    return const_iterator(id_map.end(), id_map.begin());
+  }
 
   // Modify
   std::pair<const_iterator, bool> insert(const TValue& value) {
     const auto& id = id_for_value(value);
     if (id) {
-      auto id_it = id_map.find(*id);
+      auto id_it = const_iterator(id_map.begin() + *id, id_map.begin());
       return std::pair{id_it, false};
     }
 
-    auto it = id_map.insert({next_id, value}).first;
-
-    ++next_id;
+    id_map.push_back(value);
+    auto it = const_iterator(id_map.end() - 1, id_map.begin());
 
     return std::pair{it, true};
   }
@@ -84,13 +120,12 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   std::pair<const_iterator, bool> emplace(TValue value) {
     const auto& id = id_for_value(value);
     if (id) {
-      auto id_it = id_map.find(*id);
+      auto id_it = const_iterator(id_map.begin() + *id, id_map.begin());
       return std::pair{id_it, false};
     }
 
-    auto it = id_map.insert({next_id, std::move(value)}).first;
-
-    ++next_id;
+    id_map.emplace_back(std::move(value));
+    auto it = const_iterator(id_map.end() - 1, id_map.begin());
 
     return std::pair{it, true};
   }
@@ -109,7 +144,7 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   }
 
   std::size_t erase(const TId id) {
-    if (!id_map.contains(id)) {
+    if (id < id_map.size()) {
       return 0;
     }
 
@@ -121,9 +156,9 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
   template <typename UnaryPredicate>
   std::size_t delete_all(UnaryPredicate predicate) {
     std::size_t deleted_count = 0;
-    for (const auto& [id, value] : id_map) {
-      if (predicate(value)) {
-        delete_id_and_shift(id - deleted_count);
+    for (TId i = 0; i < id_map.size(); ++i) {
+      if (predicate(id_map[i])) {
+        delete_id_and_shift(i - deleted_count);
         ++deleted_count;
       }
     }
@@ -152,16 +187,18 @@ requires(!std::same_as<TValue, TId>) class id_bimap {
     if (!id) {
       return end();
     }
-    return id_map.find(*id);
+    return const_iterator(id_map.begin() + *id, id_map.begin());
   }
 
-  const_iterator find(const TId id) const { return id_map.find(id); }
+  const_iterator find(const TId id) const {
+    return const_iterator(id_map.begin() + id, id_map.begin());
+  }
 
   template <typename UnaryPredicate>
   const_iterator find_if(UnaryPredicate predicate) const {
-    for (const auto& [key, value] : id_map) {
-      if (predicate(value)) {
-        return id_map.find(key);
+    for (TId i = 0; i < id_map.size(); ++i) {
+      if (predicate(id_map[i])) {
+        return const_iterator(id_map.begin() + i, id_map.begin());
       }
     }
     return end();
